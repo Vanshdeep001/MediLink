8ed2efc50511f1931fd0c5414d46d5a1e8ed2565
Vanshdeep Srivastava <srivastavavanshdeep18@gmail.com>
2025-09-04 04:23:32 +0000

    you haven't make any change?

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -3,9 +3,8 @@
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { app } from "./config";
 
-const auth = getAuth(app);
-
 export async function signIn(email: string, password_not_used: string) {
+  const auth = getAuth(app);
   try {
     // In a real app, you would use the password from the client.
     // For this example, we'll use a placeholder password.
