13adb893f88c9cd99d35d868bd34260791052435
Vanshdeep Srivastava <srivastavavanshdeep18@gmail.com>
2025-09-04 05:43:11 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/app/role-selection/page.tsx
+++ src/app/role-selection/page.tsx
@@ -1,16 +1,8 @@
 import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
 import { RoleSelectionForm } from "@/components/auth/role-selection-form";
-import { getCurrentUser } from "@/lib/firebase/auth";
-import { redirect } from "next/navigation";
 
 export default async function RoleSelectionPage() {
-  const user = await getCurrentUser();
-
-  if (!user) {
-    redirect('/auth');
-  }
-
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <Header />
