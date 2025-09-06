83c22dd2796daa8c2ce8b827ce4363cdeeb19e7f
Vanshdeep Srivastava <srivastavavanshdeep18@gmail.com>
2025-09-06 15:13:24 +0000

    create a normal stethoscope inside the box thats it.

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -11,27 +11,27 @@ export function Logo({ className }: { className?: string }) {
         xmlns="http://www.w3.org/2000/svg"
         className="rounded-xl"
       >
-        <defs>
-          <linearGradient
-            id="logo-gradient"
-            x1="0"
-            y1="0"
-            x2="48"
-            y2="48"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="hsl(var(--logo-color-1))" />
-            <stop offset="1" stopColor="hsl(var(--logo-color-2))" />
-          </linearGradient>
-        </defs>
         <rect width="48" height="48" rx="8" fill="hsl(var(--logo-color-3))" />
         <g
-          transform="translate(8 8) scale(1.25)"
+          transform="translate(12 12)"
           stroke="url(#logo-gradient)"
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
         >
+           <defs>
+              <linearGradient
+                id="logo-gradient"
+                x1="0"
+                y1="0"
+                x2="24"
+                y2="24"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="hsl(var(--logo-color-1))" />
+                <stop offset="1" stopColor="hsl(var(--logo-color-2))" />
+              </linearGradient>
+            </defs>
           <path d="M6 8V14C6 17.3137 8.68629 20 12 20H13" />
           <path d="M20 8V14C20 17.3137 17.3137 20 14 20H13" />
           <path d="M13 20V22C13 23.1046 13.8954 24 15 24H16C17.1046 24 18 23.1046 18 22V21.5C18 19.0147 16.9853 17 14.5 17C12.0147 17 11 19.0147 11 21.5V22C11 23.1046 10.1046 24 9 24H8C6.89543 24 6 23.1046 6 22V19" />
