3cb5470bbe130be9a46bc9c7f1a2091b1a798b10
Vanshdeep Srivastava <srivastavavanshdeep18@gmail.com>
2025-09-06 15:08:27 +0000

    this is not good at all, create something new that resmbled with healthc

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -12,63 +12,25 @@ export function Logo({ className }: { className?: string }) {
         className="rounded-xl"
       >
         <defs>
-          <linearGradient
-            id="logo-gradient-1"
-            x1="5.66895"
-            y1="8"
-            x2="20.4385"
-            y2="20.0327"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="hsl(var(--logo-color-2))" />
-            <stop offset="1" stopColor="hsl(var(--logo-color-1))" />
-          </linearGradient>
-          <linearGradient
-            id="logo-gradient-2"
-            x1="5.66895"
-            y1="28"
-            x2="20.4385"
-            y2="40.0327"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="hsl(var(--logo-color-2))" />
-            <stop offset="1" stopColor="hsl(var(--logo-color-1))" />
-          </linearGradient>
            <linearGradient
-            id="logo-gradient-3"
-            x1="25.669"
-            y1="8"
-            x2="40.4385"
-            y2="20.0327"
-            gradientUnits="userSpaceOnUse"
+            id="logo-gradient"
+            x1="0%"
+            y1="0%"
+            x2="100%"
+            y2="100%"
           >
-            <stop stopColor="hsl(var(--logo-color-2))" />
-            <stop offset="1" stopColor="hsl(var(--logo-color-1))" />
-          </linearGradient>
-           <linearGradient
-            id="logo-gradient-4"
-            x1="25.669"
-            y1="28"
-            x2="40.4385"
-            y2="40.0327"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="hsl(var(--logo-color-2))" />
-            <stop offset="1" stopColor="hsl(var(--logo-color-1))" />
+            <stop offset="0%" stopColor="hsl(var(--logo-color-1))" />
+            <stop offset="100%" stopColor="hsl(var(--logo-color-2))" />
           </linearGradient>
         </defs>
         <path
-          fillRule="evenodd"
-          clipRule="evenodd"
           d="M0 8C0 3.58172 3.58172 0 8 0H40C44.4183 0 48 3.58172 48 8V40C48 44.4183 44.4183 48 40 48H8C3.58172 48 0 44.4183 0 40V8Z"
           fill="hsl(var(--logo-color-3))"
         />
-        <g transform="translate(4, 4)">
-            <path d="M12 4C7.58172 4 4 7.58172 4 12V14C4 18.4183 7.58172 22 12 22H14V4H12Z" fill="url(#logo-gradient-1)"/>
-            <path d="M14 24H12C7.58172 24 4 27.5817 4 32V34C4 38.4183 7.58172 42 12 42H14V24Z" fill="url(#logo-gradient-2)"/>
-            <path d="M28 4C23.5817 4 20 7.58172 20 12V14C20 18.4183 23.5817 22 28 22H30V4H28Z" fill="url(#logo-gradient-3)"/>
-            <path d="M30 24H28C23.5817 24 20 27.5817 20 32V34C20 38.4183 23.5817 42 28 42H30V24Z" fill="url(#logo-gradient-4)"/>
-        </g>
+        <path 
+          d="M10 34V22.2L16.6 27.4L20.8 16L24.8 29.8L29.4 24L38 34H34.4L28.6 27L24.8 38L20.4 21.8L13.6 29.2L10 34Z" 
+          fill="url(#logo-gradient)"
+        />
       </svg>
       <div>
         <h1 className="text-2xl font-bold text-foreground tracking-tighter">MediLink</h1>
